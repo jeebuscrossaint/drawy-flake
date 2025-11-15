57c38844613b9377bfcdcd2232af5bfc11da9a84
jeebuscrossaint <thediamond270@gmail.com>
2025-11-14 23:43:56 -0500

    feat: add ability to install via nix flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-    description = "x86_64 dev environment for Drawy, the whiteboard app.";
+    description = "Drawy - A simple brainstorming tool with an infinite canvas";
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     };
@@ -9,18 +9,46 @@
         pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+        packages.${system}.default = pkgs.stdenv.mkDerivation {
+            pname = "drawy";
+            version = "0.1.0";
+
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+                cmake
+                qt6.wrapQtAppsHook
+            ];
+
+            buildInputs = with pkgs; [
+                qt6.qtbase
+                qt6.qttools
+            ];
+
+            cmakeFlags = [
+                "-DCMAKE_BUILD_TYPE=Release"
+            ];
+
+            meta = with pkgs.lib; {
+                description = "A simple brainstorming tool with an infinite canvas";
+                homepage = "https://github.com/prayag2/drawy";
+                license = licenses.gpl3Plus;
+                platforms = platforms.linux;
+                maintainers = [ ];
+            };
+        };
+
         devShells.${system}.default = pkgs.mkShell {
             buildInputs = with pkgs; [
                 qt6.full
                 qtcreator
                 cmake
                 bear
-
                 entr
             ];
-          shellHook = ''
-            which zsh >/dev/null && exec zsh
-          '';
+            shellHook = ''
+                which zsh >/dev/null && exec zsh
+            '';
         };
     };
 }
